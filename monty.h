88f95ce86b8336b8b3e5a61245ab92478851cbe3
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-02 22:33:04 -0500

    update-remove

--- monty.h
+++ monty.h
@@ -28,10 +28,6 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-
-
-
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -46,8 +42,6 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/* Function read, parse, execute */
-
 /*Functions Monty*/
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
@@ -56,8 +50,5 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
-
-
-
 
 #endif /* MONTY_H */
