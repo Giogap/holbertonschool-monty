af0afc8cc8952c17cb7047fc48f6b2e829b3f6c6
SilvanaJ90 <silvanajaramillo90@gmail.com>
2022-09-03 18:37:08 -0500

    update-main

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 *Return: always 0
 */
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
 	FILE *file;
 	char *date;
@@ -17,15 +17,15 @@ int main(int argc, char **argv)
 	stack_t *head = NULL;
 
 	initfonc(structfonc);
-	if (argc != 2)
+	if (ac != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
+	file = fopen(av[1], "r");
 	if (!file)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (fgets(line, sizeof(line), file) != NULL)
